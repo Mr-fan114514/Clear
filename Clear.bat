e7b0f92292c6aadcb3f030ae05b717b6c32f332f
Mr-fan114514 <1715845204@qq.com>
2024-07-11 20:14:44 +0800

    Add files via upload

---
+++ Clear.bat
@@ -0,0 +1,17 @@
+@ECHO off
+echo Windows Clear
+cd C:\Windows 
+del Temp
+cd %USERPROFILE%\AppData\Local\Temp
+del %USERPROFILE%\AppData\Local\Temp
+cd C:\Windows\Prefetc=
+del C:\Windows\Prefetch
+cd C/Windows/WinSxS/Backup
+del C/Windows/WinSxS/Backup
+cd C:\Windows\System32\LogFiles
+del C:\Windows\System32\LogFiles
+cd C:\Windows\SoftwareDistribution
+del C:\Windows\SoftwareDistribution
+cd C:\Windows\Installer
+del C:\Windows\Installer
+ 
